0c4956dd8ce807d50da6530cd24894f28ef90515
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-03-05 16:58:23 -0500

    Creat FileUploadService
    Will need to write tests to verify that it works well. Probably need to
    write a util method to split a File into parts and to test the service
    with it.

---
+++ app/service/FileUploadService.scala
@@ -0,0 +1,48 @@
+package service
+
+import java.io.RandomAccessFile
+import model._
+
+import java.util.concurrent.{ ConcurrentMap, ConcurrentHashMap }
+import scala.collection.JavaConversions._
+
+class FileUploadService(serviceSavePath: String) {
+
+	val basePath = if (serviceSavePath.endsWith("/")) {
+		serviceSavePath
+	} else { serviceSavePath + "/" }
+
+	val uploadedParts: ConcurrentMap[String, Set[FileUploadInfo]] = new ConcurrentHashMap(8, 0.9f, 1)
+
+	def fileNameFor(fileInfo: FileUploadInfo) = {
+		s"${basePath}${fileInfo.resumableIdentifier}-${fileInfo.resumableFilename}"
+	}
+
+	def savePartialFile(filePart: Array[Byte], fileInfo: FileUploadInfo) {
+		val partialFile = new RandomAccessFile(fileNameFor(fileInfo), "rw")
+		val offset = (fileInfo.resumableChunkNumber - 1) * fileInfo.resumableChunkSize
+
+		partialFile.seek(offset)
+		partialFile.write(filePart, offset, filePart.length)
+		partialFile.close()
+
+		val key = fileNameFor(fileInfo)
+		if (uploadedParts.containsKey(key)) {
+			val partsUploaded = uploadedParts.get(key)
+			uploadedParts.put(key, partsUploaded + fileInfo)
+		} else {
+			uploadedParts.put(key, Set(fileInfo))
+		}
+	}
+
+	def isUploadComplete(fileInfo: FileUploadInfo): Boolean = {
+		val key = fileNameFor(fileInfo)
+		val possibleFinishedFile = new RandomAccessFile(key, "r")
+		val fileLength = possibleFinishedFile.length()
+		if (fileLength == fileInfo.resumableTotalSize && uploadedParts.containsKey(key)) {
+			uploadedParts.get(key).size == fileInfo.totalChunks
+		}
+		return false
+	}
+
+}
