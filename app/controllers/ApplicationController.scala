590e703eeb6431bcd9cbba00d62ce5f44f9ffe23
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-03-24 01:18:07 -0400

    Rough controller for upload part
    Not perfect yet, need to use tmpfiles and check for file completion as
    well.

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -7,6 +7,9 @@ import play.api.Logger
 import service._
 import form._
 
+import java.io.File
+import java.nio.file.Files
+
 object FileUploadController extends Controller {
 
 	val fileUploadService = new FileUploadService("tmp/")
@@ -15,14 +18,30 @@ object FileUploadController extends Controller {
 		Ok(views.html.index())
 	}
 
-	def upload = Action {
-		BadRequest("Ok")
+	def upload = Action(parse.multipartFormData) { implicit request =>
+		Forms.fileUploadInfoForm.bindFromRequest.fold(
+			formWithErrors => {
+				BadRequest(formWithErrors.errors.mkString("\n"))
+			},
+			fileUploadInfo => {
+				request.body.file("file") match {
+					case None => BadRequest("No file")
+					case Some(file) =>
+						val tmpName = Play.application(Play.current).path.getAbsolutePath + "/" + file.filename
+						file.ref.moveTo(new File(tmpName))
+						val bytes = Files.readAllBytes(new File(tmpName) toPath ())
+						fileUploadService.savePartialFile(bytes, fileUploadInfo)
+						Ok
+				}
+			}
+		)
+
 	}
 
 	def uploadTest = Action { implicit request =>
 		Forms.fileUploadInfoForm.bindFromRequest.fold(
 			formWithErrors => {
-				BadRequest(formWithErrors.toString)
+				BadRequest(formWithErrors.errors.mkString("\n"))
 			},
 			fileUploadInfo => {
 				if (fileUploadService.isPartialUploadComplete(fileUploadInfo)) {
