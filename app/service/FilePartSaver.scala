18efb37e7e844353d4edebf670a5cfce880f06aa
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-05-03 17:44:33 -0400

    Add single instance implementation for FilePartSaver

--- app/service/FilePartSaver.scala
+++ app/service/FilePartSaver.scala
@@ -11,3 +11,22 @@ trait FilePartSaver {
 
 	def put(key: String, filePart: FilePart): Boolean
 }
+
+object SingleInstanceFilePartSaver extends FilePartSaver {
+	val uploadedParts: ConcurrentMap[String, Set[FileUploadInfo]] = new ConcurrentHashMap(8, 0.9f, 1)
+
+	def get(key: String) = {
+		if (uploadedParts.containsKey(key)) {
+			Some(uploadedParts.get(key))
+		} else {
+			None
+		}
+	}
+
+	def contains(key: String) = uploadedParts.contains(key)
+
+	def put(key: String, filePart: FilePart) = {
+		uploadedParts.put(key, filePart.fileInfo) //Because this is backed by a single instance we don't store the byte array here
+		uploadedParts.containsKey(key)
+	}
+}
