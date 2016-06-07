b00933c0b18614e3ee2c9b04b35acf4d9ff4263b
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2016-06-07 08:00:17 -0400

    Add response classes

--- app/actors/UploadResponses.scala
+++ app/actors/UploadResponses.scala
@@ -1,5 +1,10 @@
 package actors
 
 import model._
-
 sealed trait UploadResponses
+
+case class FilePartAdded() extends UploadResponses
+
+case class FilePartExists() extends UploadResponses
+
+case class FileComplete() extends UploadResponses
